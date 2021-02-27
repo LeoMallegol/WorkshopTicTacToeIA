6d9cf191791f77a8b6743b2f9f5c97118a83dcbe
Leo <leo.mallegol@epitech.eu>
2021-02-27 11:14:16 +0100

    Add : true ia

--- ia.py
+++ ia.py
@@ -3,11 +3,79 @@ class IA(object):
     def __init__(self):
         pass
 
-    def evaluateBoard(self, board) :
-        pass
+    def endGame(self, board) :
+        if (board[0] == board[1] == board[2] and board[0] != 0) :
+            return 1
+        elif (board[3] == board[4] == board[5] and board[3] != 0) :
+            return 1
+        elif (board[6] == board[7] == board[8] and board[6] != 0) :
+            return 1
+        elif (board[0] == board[3] == board[6] and board[0] != 0) :
+            return 1
+        elif (board[1] == board[4] == board[7] and board[1] != 0) :
+            return 1
+        elif (board[2] == board[5] == board[8] and board[2] != 0) :
+            return 1
+        elif (board[0] == board[4] == board[8] and board[0] != 0) :
+            return 1
+        elif (board[2] == board[4] == board[6] and board[2] != 0) :
+            return 1
+        elif (board.count(0) == 0) :
+            return 1
+        return 0
 
-    def Minimax(self, board) :
-        pass
+    def evaluateBoard(self, board) :
+        if (board[0] == board[1] == board[2] and board[0] != 0) :
+            return (-1, 1)[board[0] == 2]
+        elif (board[3] == board[4] == board[5] and board[3] != 0) :
+            return (-1, 1)[board[3] == 2]
+        elif (board[6] == board[7] == board[8] and board[6] != 0) :
+            return (-1, 1)[board[6] == 2]
+        elif (board[0] == board[3] == board[6] and board[0] != 0) :
+            return (-1, 1)[board[0] == 2]
+        elif (board[1] == board[4] == board[7] and board[1] != 0) :
+            return (-1, 1)[board[1] == 2]
+        elif (board[2] == board[5] == board[8] and board[2] != 0) :
+            return (-1, 1)[board[2] == 2]
+        elif (board[0] == board[4] == board[8] and board[0] != 0) :
+            return (-1, 1)[board[0] == 2]
+        elif (board[2] == board[4] == board[6] and board[2] != 0) :
+            return (-1, 1)[board[2] == 2]
+        elif (board.count(0) == 0) :
+            return 0
+
+    def giveEmptyCases(self, board) :
+        return [i for i in range(len(board)) if board[i] == 0]
+
+    def Minimax(self, board, isIA) :
+        if self.endGame(board) :
+            return (self.evaluateBoard(board), 1)
+        elif isIA :
+            value = float("-inf")
+            bestCase = 0
+            emptyCases = self.giveEmptyCases(board)
+            for i in emptyCases :
+                board[i] = 2
+                tmpValue = self.Minimax(board, False)[0]
+                if (value < tmpValue) :
+                    value = tmpValue
+                    bestCase = i
+                board[i] = 0
+            return value, bestCase
+        else :
+            value = float("inf")
+            worstCase = 0
+            emptyCases = self.giveEmptyCases(board)
+            for i in emptyCases :
+                board[i] = 1
+                tmpValue = self.Minimax(board, True)[0]
+                if (value > tmpValue) :
+                    value = tmpValue
+                    worstCase = i
+                board[i] = 0
+            return value, worstCase
 
     def getTurn(self, board) :
-        return board.index(0) + 1
+        a = self.Minimax(board, True)
+        print(a[0])
+        return a[1] + 1
